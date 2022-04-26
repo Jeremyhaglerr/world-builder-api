9e9a29baf795479d3f320a6167d82ed0c3a30901
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-04-26 15:47:35 -0500

    add update and delete functionality

--- api/views/characters.py
+++ api/views/characters.py
@@ -27,3 +27,32 @@ def index():
 def show(id):
   character = Character.query.filter_by(id=id).first()
   return jsonify(character.serialize()), 200
+
+@characters.route('/<id>', methods=["PUT"]) 
+@login_required
+def update(id):
+  data = request.get_json()
+  profile = read_token(request)
+  character = Character.query.filter_by(id=id).first()
+
+  if character.profile_id != profile["id"]:
+    return 'Forbidden', 403
+
+  for key in data:
+    setattr(character, key, data[key])
+
+  db.session.commit()
+  return jsonify(character.serialize()), 200
+
+@characters.route('/<id>', methods=["DELETE"]) 
+@login_required
+def delete(id):
+  profile = read_token(request)
+  character = Character.query.filter_by(id=id).first()
+
+  if character.profile_id != profile["id"]:
+    return 'Forbidden', 403
+    
+  db.session.delete(character)
+  db.session.commit()
+  return jsonify(message="Success"), 200
